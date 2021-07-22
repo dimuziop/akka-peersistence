3e7e5a8f6c3760ce308cb1ffa81b86dd9ec54b10
Pat <dimuziop@gmail.com>
2021-07-22 13:56:54 +0200

    problem solution 1 applied: smelly -> starting solution 2

--- src/main/scala/part4_practices/EventAdapters.scala
+++ src/main/scala/part4_practices/EventAdapters.scala
@@ -4,7 +4,6 @@ import akka.actor.{ActorLogging, ActorSystem, Props}
 import akka.persistence.PersistentActor
 import com.typesafe.config.ConfigFactory
 
-import java.util.UUID
 import scala.collection.mutable
 
 /**
@@ -24,7 +23,8 @@ object EventAdapters extends App {
   case class AddGuitar(guitar: Guitar, quantity: Int)
 
   // event
-  case class GuitarAdded(guitarId: String, guitarModel: String, guitarMake: String, quantity: Int, guitarType: String)
+  case class GuitarAdded(guitarId: String, guitarModel: String, guitarMake: String, quantity: Int)
+  case class GuitarAddedV2(guitarId: String, guitarModel: String, guitarMake: String, quantity: Int, guitarType: String)
 
   class InventoryManager extends PersistentActor with ActorLogging {
     override def persistenceId: String = "guitar-inventory-manager"
@@ -33,7 +33,7 @@ object EventAdapters extends App {
 
     override def receiveCommand: Receive = {
       case AddGuitar(guitar@Guitar(id, model, make, guitarType), quantity) =>
-        persist(GuitarAdded(id, model, make, quantity, guitarType)) { _ =>
+        persist(GuitarAddedV2(id, model, make, quantity, guitarType)) { _ =>
           updateInventory(guitar, quantity)
           log.info(s"Added the $quantity x $guitar to inventory")
         }
@@ -42,10 +42,14 @@ object EventAdapters extends App {
     }
 
     override def receiveRecover: Receive = {
-      case GuitarAdded(id, model, make, quantity, guitarType) =>
-        val guitar: Guitar = Guitar(id, model, make, guitarType)
+      case GuitarAdded(id, model, make, quantity) =>
+        val guitar: Guitar = Guitar(id, model, make, ACOUSTIC)
         updateInventory(guitar, quantity)
         log.info(s"Recovered the $quantity x $guitar to inventory")
+      case event @ GuitarAddedV2(id, model, make, quantity, guitarType) =>
+        val guitar: Guitar = Guitar(id, model, make, guitarType)
+        updateInventory(guitar, quantity)
+        log.info(s"Recovered the $event")
     }
 
     private def updateInventory(guitar: Guitar, quantity: Int): Unit = {
@@ -57,10 +61,12 @@ object EventAdapters extends App {
   val system = ActorSystem("eventAdapters", ConfigFactory.load().getConfig("eventAdapters"))
   val inventoryManager = system.actorOf(Props[InventoryManager], "inventoryManager")
 
-  val guitars = for (i <- 1 to 10) yield Guitar(UUID.randomUUID().toString, s"JVMBlueSpecial $i", "Brian June")
+  /*val guitars = for (i <- 1 to 10) yield Guitar(UUID.randomUUID().toString, s"JVMBlueSpecial $i", "Brian June")
   guitars.foreach { guitar =>
     inventoryManager ! AddGuitar(guitar, 5)
-  }
+  }*/
+
+  inventoryManager ! "print"
 
 
 
